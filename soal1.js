488a5b69519c2dc98340bb167efa4b2862a80a2c
hendraw <emailhendra2@gmail.com>
2023-01-30 14:35:01 +0700

    fix

--- soal1.js
+++ soal1.js
@@ -1,5 +1,19 @@
-const number = 50;
-let output;
-for (let i = 0; i < number; i++) {
-    output = output ? `${output}${i},` : `${i},`;
+function test ()  {
+    const result = []
+    for(let i = 1; i <= 50; i += 1) {
+        if(i % 3 === 0 || i % 5 === 0) {
+            if(i%3 === 0) {
+                result.push('Frontend')
+            }
+            if(i%5 === 0) {
+                result.push('Backend')
+            }
+        } else {
+            result.push(i)
+        }
+    }
+
+    return result.join(',')
 }
+
+console.log(test())
