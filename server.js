3b5057fcfcf4f6905c6dd3a684168dc3854dda87
hendraw <emailhendra2@gmail.com>
2023-01-30 14:16:49 +0700

    update

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+const data = require('./sku.json');
+
+app.get('/', function(req, res) {
+  let rows = data;
+  if(req.query.keyword){
+    rows = data.filter((field)=>field.item_name.toLowerCase().includes(req.query.keyword));
+  }
+  rows = rows.sort((a,b)=>{
+    return b.item_name - a.item_name; 
+  });
+  res.send({
+    'data': rows,
+    'count': rows.length
+  });
+});
+
+app.listen(port);
+console.log('Server started at ' + port);
